b7e8dc04552686c92f3d06608bef4a1df0ea9430
Daniel Merrill <daniel.r.merrill@gmail.com>
2019-01-19 20:35:44 -0800

    [Dot] Allocate ring/placeholder per dot

--- screens/Dot.tsx
+++ screens/Dot.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Dimensions, Image, Text, View, StyleSheet, TouchableOpacity, StatusBar, Platform } from 'react-native';
 import Animated, { Easing } from 'react-native-reanimated';
 import { PanGestureHandler, State, TapGestureHandler } from 'react-native-gesture-handler';
-import { throttle } from 'lodash'
 
 import BackButton from '../components/BackButton'
 
@@ -10,18 +9,21 @@ const { width, height } = Dimensions.get("window")
 
 const dotSize = 80
 const additionalScale = 0.5
+const numDots = 7
 const ringScales = {
   disabled: 0,
   in: 2,
   out: 15,
 }
 
-const duration = {
-  active: 500,
-  inactive: 1000,
-}
+const dotCenterX = width / 2 - dotSize / 2
+const dotCenterY = height / 2 - dotSize / 2
 
-const {
+const dropZoneRadius = width / 3
+const translateX = width / 2 - dropZoneRadius / 2
+const translateY = height / 2 - dropZoneRadius / 2
+
+let {
   onChange,
   debug,
   and,
@@ -60,7 +62,11 @@ class Dot extends Component {
   constructor(props) {
     super(props);
 
-
+    const xl = width / 2 - dropZoneRadius / 2
+    const xr = xl + dropZoneRadius
+    const yt = height / 2 - dropZoneRadius / 2
+    const yb = yt + dropZoneRadius
+    const dotRadius = dotSize * (1 + additionalScale)
 
     // Center drop zone rotate/scale animation
     const rotClock = new Clock()
@@ -76,7 +82,8 @@ class Dot extends Component {
       toValue: Math.PI * 2,
       duration: 20000,
     }
-    this.centerRotate = block([
+
+    this.dropZoneRotate = block([
       cond(not(clockRunning(rotClock)), startClock(rotClock)),
       timing(rotClock, rotState, rotConfig),
       cond(rotState.finished, [
@@ -90,7 +97,7 @@ class Dot extends Component {
       rotState.position,
     ])
 
-    const scaleClock = new Clock()
+    const dropZoneClock = new Clock()
 
     const scaleState = {
       position: new Value(0),
@@ -107,110 +114,20 @@ class Dot extends Component {
     this.scaleVal = new Value(0.05)
 
     const runScale = block([
-      cond(not(clockRunning(scaleClock)), startClock(scaleClock)),
-      timing(scaleClock, scaleState, this.scaleConfig),
+      cond(not(clockRunning(dropZoneClock)), startClock(dropZoneClock)),
+      timing(dropZoneClock, scaleState, this.scaleConfig),
       cond(scaleState.finished, [
-        stopClock(scaleClock),
+        stopClock(dropZoneClock),
         set(scaleState.position, 0),
         set(scaleState.time, 0),
         set(scaleState.frameTime, 0),
         set(scaleState.finished, 0),
-        startClock(scaleClock),
+        startClock(dropZoneClock),
       ]),
       scaleState.position,
     ])
 
-    this.centerScale = add(1, multiply(this.scaleVal, sin(runScale)))
-
-
-
-
-    // Ring animation
-    const ringR = new Value(0)
-    const ringG = new Value(0)
-    const ringB = new Value(0)
-    const ringA = new Value(1)
-
-    this.ring = {
-      r: ringR,
-      g: ringG,
-      b: ringB,
-      a: ringA,
-      color: color(ringR, ringG, ringB, ringA),
-      rgb: {
-        r: ringR,
-        g: ringG,
-        b: ringB,
-        a: ringA,
-      },
-    }
-
-    this.ringClock = new Clock()
-    this.ringColor = color()
-    this.ringState = {
-      position: new Value(0),
-      time: new Value(0),
-      finished: new Value(0),
-      velocity: new Value(0),
-    }
-    this.ringConfig = {
-      damping: 22,
-      mass: 1,
-      stiffness: 55,
-      overshootClamping: false,
-      restSpeedThreshold: 0.001,
-      restDisplacementThreshold: 0.001,
-      toValue: new Value(ringScales.in),
-    }
-
-    const resetRing = [
-      set(this.ringState.position, ringScales.disabled),
-      set(this.ringState.time, 0),
-      set(this.ringState.finished, 0),
-      set(this.ringState.velocity, 0)
-    ]
-
-    this.ringScale = block([
-      cond(clockRunning(this.ringClock), [
-        spring(this.ringClock, this.ringState, this.ringConfig),
-        cond(this.ringState.finished, [
-          stopClock(this.ringClock),
-          set(this.ringState.time, 0),
-          set(this.ringState.velocity, 0),
-          set(this.ringState.finished, 0),
-          // resetRing,
-        ])
-      ]),
-      this.ringState.position,
-    ])
-
-
-
-
-
-
-
-    const numDots = 7
-    this.radius = width / 3
-    const dotCenterX = width / 2 - dotSize / 2
-    const dotCenterY = height / 2 - dotSize / 2
-
-    const placeholderR = new Value(0)
-    const placeholderG = new Value(0)
-    const placeholderB = new Value(0)
-    const placeholderA = new Value(0)
-
-    this.placeholderDot = {
-        x: new Value(0),
-        y: new Value(0),
-        r: placeholderR,
-        g: placeholderG,
-        b: placeholderB,
-        a: placeholderA,
-        color: color(placeholderR, placeholderG, placeholderB, placeholderA),
-        opacity: 1,
-        scale: new Value(0),
-    }
+    this.dropZoneScale = add(1, multiply(this.scaleVal, sin(runScale)))
 
     this.dots = [...Array(numDots)].map((d, index, arr) => {
       const colorMultiplier = 255 / (arr.length - 1)
@@ -221,8 +138,8 @@ class Dot extends Component {
       const dragY = new Value(0);
       const ratio = index / arr.length
 
-      const startX = new Value(dotCenterX + Math.sin(ratio * Math.PI * 2) * this.radius)
-      const startY = new Value(dotCenterY + Math.cos(ratio * Math.PI * 2) * this.radius)
+      const startX = new Value(dotCenterX + Math.sin(ratio * Math.PI * 2) * dropZoneRadius)
+      const startY = new Value(dotCenterY + Math.cos(ratio * Math.PI * 2) * dropZoneRadius)
       
       const prevX = new Value(0);
       const prevY = new Value(0);
@@ -277,11 +194,6 @@ class Dot extends Component {
         translate: add(startY, multiply(runClock, add(dragY, prevY)))
       }
 
-      const xl = width / 2 - this.radius / 2
-      const xr = xl + this.radius
-      const yt = height / 2 - this.radius / 2
-      const yb = yt + this.radius
-      const dotRadius = dotSize * (1 + additionalScale)
       const dotCenter = {
         x: add(x.translate, dotRadius / 2),
         y: add(y.translate, dotRadius / 2,)
@@ -309,11 +221,86 @@ class Dot extends Component {
         duration: 1000,
         toValue: 1,
       }
+      // Ring
+      // Ring animation
+      const ringR = new Value(0)
+      const ringG = new Value(0)
+      const ringB = new Value(0)
+      const ringA = new Value(0)
+
+
+
+      const ringClock = new Clock()
+      const ringState = {
+        position: new Value(0),
+        time: new Value(0),
+        finished: new Value(0),
+        velocity: new Value(0),
+      }
+      const ringConfig = {
+        damping: 22,
+        mass: 1,
+        stiffness: 55,
+        overshootClamping: false,
+        restSpeedThreshold: 0.001,
+        restDisplacementThreshold: 0.001,
+        toValue: new Value(ringScales.in),
+      }
+
+      const ringScale = block([
+        cond(clockRunning(ringClock), [
+          spring(ringClock, ringState, ringConfig),
+          cond(ringState.finished, [
+            stopClock(ringClock),
+            set(ringState.time, 0),
+            set(ringState.velocity, 0),
+            set(ringState.finished, 0),
+          ])
+        ]),
+        ringState.position,
+      ])
+
+      const ring = {
+        clock: ringClock,
+        state: ringState,
+        config: ringConfig,
+        scale: ringScale,
+        r: ringR,
+        g: ringG,
+        b: ringB,
+        a: ringA,
+        color: color(ringR, ringG, ringB, ringA),
+        rgb: {
+          r: ringR,
+          g: ringG,
+          b: ringB,
+          a: ringA,
+        },
+      }
+
+      // Placeholder
+      const placeholderR = new Value(0)
+      const placeholderG = new Value(0)
+      const placeholderB = new Value(0)
+      const placeholderA = new Value(0)
+
+      const placeholder = {
+        x: new Value(0),
+        y: new Value(0),
+        r: placeholderR,
+        g: placeholderG,
+        b: placeholderB,
+        a: placeholderA,
+        color: color(placeholderR, placeholderG, placeholderB, placeholderA),
+        opacity: 1,
+        scale: new Value(0),
+      }
 
       const runEndClock = [
         timing(endClock, endState, endConfig),
         cond(endState.finished, [
           stopClock(endClock),
+          set(placeholder.a, 0),
           set(endState.finished, 0),
           set(endState.time, 0),
           set(endState.frameTime, 0),
@@ -323,7 +310,7 @@ class Dot extends Component {
 
       const scale = cond(
         clockRunning(endClock), [
-          set(this.placeholderDot.scale, multiply(add(1, additionalScale), add(1, multiply(-1, runEndClock)))),
+          set(placeholder.scale, multiply(add(1, additionalScale), add(1, multiply(-1, runEndClock)))),
           runEndClock, 
           ], [
           cond(eq(endState.position, 1), set(endState.position, 0)),
@@ -337,29 +324,34 @@ class Dot extends Component {
         intersects,
         x,
         y,
-        scale,
-        dotScaleState,
-        dotScaleConfig,
+        scale: {
+          clock: clock,
+          value: scale,
+          state: dotScaleState,
+          config: dotScaleConfig,
+        },
         clock,
         endClock,
-        dotColor,
+        color: dotColor,
         rgb: {r, g, b},
+        placeholder,
+        ring,
       }
     })
   }
 
   renderDot = ({ 
-    dotColor,
-    gestureState, 
     x,
     y,
-    clock, 
-    endClock,
+    color,
+    rgb,
     scale, 
-    dotScaleConfig,
+    gestureState, 
+    endClock,
     zIndex,
     intersects,
-    rgb,
+    placeholder,
+    ring,
   }, i) => {
 
     return (
@@ -372,34 +364,34 @@ class Dot extends Component {
               cond(
                 and(
                   intersects, 
-                  not(clockRunning(this.ringClock)),
-                  neq(this.ringState.position, ringScales.in),
+                  not(clockRunning(ring.clock)),
+                  neq(ring.state.position, ringScales.in),
                 ), 
                 [
-                  debug('entering center', this.ringState.position),
-                  set(this.ring.a, 1),
-                  set(this.ringState.position, ringScales.disabled),
-                  set(this.ringConfig.toValue, ringScales.in),
-                  startClock(this.ringClock),
+                  debug('entering center', ring.state.position),
+                  set(ring.a, 1),
+                  set(ring.state.position, ringScales.disabled),
+                  set(ring.config.toValue, ringScales.in),
+                  startClock(ring.clock),
                 ]
               ),
               // Dot leaving center
               cond(
                 and(
                   not(intersects),
-                  not(clockRunning(this.ringClock)),
-                  neq(this.ringState.position, ringScales.disabled),
+                  not(clockRunning(ring.clock)),
+                  neq(ring.state.position, ringScales.disabled),
                 ), [
-                  debug('leaving center', this.ringState.position),
-                  set(this.ringConfig.toValue, ringScales.disabled),
-                  startClock(this.ringClock),
+                  debug('leaving center', ring.state.position),
+                  set(ring.config.toValue, ringScales.disabled),
+                  startClock(ring.clock),
                 ]
               ),
               set(x.drag, translationX),
               set(y.drag, translationY),
               cond(intersects, [
-                set(this.placeholderDot.x, x.translate),
-                set(this.placeholderDot.y, y.translate),
+                set(placeholder.x, x.translate),
+                set(placeholder.y, y.translate),
               ]),
             ])
           ]),
@@ -412,19 +404,29 @@ class Dot extends Component {
                 neq(gestureState, State.ACTIVE),
                 eq(state, State.ACTIVE),
               ), [
-                debug('becoming active', dotScaleConfig.toValue),
-                set(this.ring.a, 0), // Hide ring spring back to center
-                set(this.ring.r, rgb.r),
-                set(this.ring.g, rgb.g),
-                set(this.ring.b, rgb.b),
-                set(this.placeholderDot.r, rgb.r),
-                set(this.placeholderDot.g, rgb.g),
-                set(this.placeholderDot.b, rgb.b),
-                set(this.placeholderDot.a, 0),
+                debug('becoming active', scale.config.toValue),
+                set(ring.a, 1), // Hide ring spring back to center
+                set(ring.r, rgb.r),
+                set(ring.g, rgb.g),
+                set(ring.b, rgb.b),
+                set(placeholder.r, rgb.r),
+                set(placeholder.g, rgb.g),
+                set(placeholder.b, rgb.b),
+                set(placeholder.a, 0),
                 set(zIndex, 999),
-                set(dotScaleConfig.toValue, 1),
-                set(this.placeholderDot.scale, 0.5),
-                startClock(clock),
+                set(scale.config.toValue, 1),
+                set(placeholder.scale, 0.5),
+                cond(or(
+                  clockRunning(ring.clock),
+                  neq(ring.state.position, ringScales.disabled),
+                ), [
+                  stopClock(ring.clock),
+                  set(ring.state.position, ringScales.disabled),
+                  set(ring.state.finished, 0),
+                  set(ring.state.velocity, 0),
+                  set(ring.state.time, 0),
+                ]),
+                startClock(scale.clock),
               ]
             ),
             // Dot becoming inactive
@@ -433,14 +435,19 @@ class Dot extends Component {
                 eq(gestureState, State.ACTIVE),
                 neq(state, State.ACTIVE),
               ), [
-                debug('becoming inactive', dotScaleConfig.toValue),
-                set(this.placeholderDot.a, 1),
+                debug('becoming inactive', scale.config.toValue),
+                set(placeholder.a, 1),
                 set(zIndex, 0),
-                set(dotScaleConfig.toValue, 0),
-                cond(intersects, startClock(endClock)),
-                startClock(clock),
-                set(this.ringConfig.toValue, cond(intersects, ringScales.out, ringScales.disabled)),
-                startClock(this.ringClock),
+                set(scale.config.toValue, 0),
+                cond(intersects, 
+                  startClock(endClock),
+                  [
+                    set(placeholder.a, 0),
+                  ]
+                ),
+                startClock(scale.clock),
+                set(ring.config.toValue, cond(intersects, ringScales.out, ringScales.disabled)),
+                startClock(ring.clock),
               ]),
             set(gestureState, state),
           ])
@@ -467,10 +474,10 @@ class Dot extends Component {
               width: dotSize,
               height: dotSize,
               borderRadius: dotSize / 2,
-              backgroundColor: dotColor,
+              backgroundColor: color,
               transform: [{
-                scaleX: scale,
-                scaleY: scale,
+                scaleX: scale.value,
+                scaleY: scale.value,
               }]
             }}
           />
@@ -480,62 +487,69 @@ class Dot extends Component {
     )
   }
 
-  renderPlaceholder = () => (
+  renderPlaceholder = ({ placeholder }, i) => (
     <Animated.View
+      key={`placeholder-${i}`}
       style={{
         position: 'absolute',
         opacity: 0.85,
         width: dotSize,
         height: dotSize,
         borderRadius: dotSize / 2,
-        backgroundColor: this.placeholderDot.color,
+        backgroundColor: placeholder.color,
         zIndex: 999,
         transform: [{
-          translateX: this.placeholderDot.x,
-          translateY: this.placeholderDot.y,
-          scaleX: this.placeholderDot.scale,
-          scaleY: this.placeholderDot.scale,
+          translateX: placeholder.x,
+          translateY: placeholder.y,
+          scaleX: placeholder.scale,
+          scaleY: placeholder.scale,
         }]
       }}
     />
   )
 
+  renderRing = ({ ring }, i) => {
+    return (
+      <Animated.View
+        key={`ring-${i}`}
+        style={{
+          position: 'absolute',
+          opacity: 0.85,
+          width: dropZoneRadius,
+          height: dropZoneRadius,
+          borderRadius: dropZoneRadius,
+          borderColor: ring.color,
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderWidth: 20,
+          transform: [{
+            translateX,
+            translateY,
+            scaleX: ring.scale,
+            scaleY: ring.scale,
+          }]
+        }}
+      />
+    )
+  }
+
   render() {
-    const translateX = width / 2 - this.radius / 2
-    const translateY = height / 2 - this.radius / 2
+
 
     return (
       <Animated.View style={[
         styles.container,
       ]}>
-        <Animated.View
-          style={{
-            position: 'absolute',
-            opacity: 0.85,
-            width: this.radius,
-            height: this.radius,
-            borderRadius: this.radius,
-            borderColor: this.ring.color,
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderWidth: 20,
-            transform: [{
-              translateX,
-              translateY,
-              scaleX: this.ringScale,
-              scaleY: this.ringScale,
-            }]
-          }}
-        />
 
-        {this.renderPlaceholder()}
+        {this.dots.map(this.renderRing)}
+        {this.dots.map(this.renderPlaceholder)}
 
       <Animated.View
         style={{
           position: 'absolute',
-          width: this.radius,
-          height: this.radius,
-          borderRadius: this.radius,
+          width: dropZoneRadius,
+          height: dropZoneRadius,
+          borderRadius: dropZoneRadius,
           alignItems: 'center',
           justifyContent: 'center',
           borderWidth: 5,
@@ -545,9 +559,9 @@ class Dot extends Component {
           transform: [{
             translateX,
             translateY,
-            rotate: this.centerRotate,
-            scaleX: this.centerScale,
-            scaleY: this.centerScale,
+            rotate: this.dropZoneRotate,
+            scaleX: this.dropZoneScale,
+            scaleY: this.dropZoneScale,
           }]
         }}
       >
